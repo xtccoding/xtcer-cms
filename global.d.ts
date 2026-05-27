a19190a30475fc7191c8fa8a07e3a9149ecc76cd
ctoox <ctoocn@gmail.com>
2026-05-28 04:36:12 +0800

    fix: add type declaration for .scss imports

--- global.d.ts
+++ global.d.ts
@@ -3,6 +3,11 @@ declare module '*.css' {
   export default content
 }
 
+declare module '*.scss' {
+  const content: Record<string, string>
+  export default content
+}
+
 declare module '@payloadcms/next/css' {
   const content: Record<string, string>
   export default content
