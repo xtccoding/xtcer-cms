10b0512db3e852284755a3a3c90c2e563e424aa0
ctoox <ctoocn@gmail.com>
2026-05-28 04:19:38 +0800

    fix: layout css import

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles.css'
+import './styles.css' // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
