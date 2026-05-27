d7e12714b1496d09b99e0a32fa870512ef488a01
ctoox <ctoocn@gmail.com>
2026-05-28 04:30:04 +0800

    fix: add type declaration for @payloadcms/next/css

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,8 @@ declare module '*.css' {
   const content: Record<string, string>
   export default content
 }
+
+declare module '@payloadcms/next/css' {
+  const content: Record<string, string>
+  export default content
+}
